834b2a39685c076decb16b2dc97d8e9f4da6a078
Shaun Hammill <shammill@traumasoft.com>
2021-06-18 09:42:57 -0400

    Building some bones

--- cmd/menace-bot/menace-bot.go
+++ cmd/menace-bot/menace-bot.go
@@ -2,9 +2,14 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/Plloi/pdb-cmdr/pkg/router"
+	"github.com/Plloi/pdb-menace/pkg/menace"
+	"github.com/bwmarrin/discordgo"
 	"github.com/joho/godotenv"
 
 	log "github.com/sirupsen/logrus"
@@ -31,4 +36,41 @@ func init() {
 
 func main() {
 	// TODO Basic Bot to play Menace
+	log.Info("Starting up")
+	// Create a new Discord session using the provided bot token.
+	if Token == "" {
+		log.Error("Token Required for bot usage")
+		os.Exit(1)
+	}
+	dg, err := discordgo.New("Bot " + Token)
+	if err != nil {
+		fmt.Println("error creating Discord session,", err)
+		return
+	}
+
+	Router = router.NewCommandRouter()
+	Router.DefaultPrefix = "m!"
+	Router.RegisterCommand("prefix", "Sets the bot command prefix (Admin Locked)", Router.SetPrefix)
+
+	// Register router's command handler for message events.
+	dg.AddHandler(Router.HandleCommand)
+
+	// Open a websocket connection to Discord and begin listening.
+	err = dg.Open()
+	if err != nil {
+		fmt.Println("error opening connection,", err)
+		return
+	}
+
+	// Load Menace Commands
+	menace.Setup(Router)
+
+	// Wait here until CTRL-C or other term signal is received.
+	log.Info("Bot is now running")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	// Cleanly close down the Discord session.
+	dg.Close()
 }
--- pkg/menace/menace.go
+++ pkg/menace/menace.go
@@ -1,1 +1,17 @@
 package menace
+
+import (
+	"github.com/Plloi/pdb-cmdr/pkg/router"
+	"github.com/bwmarrin/discordgo"
+	log "github.com/sirupsen/logrus"
+)
+
+// Setup Registers package commands to a command router
+func Setup(r *router.CommandRouter) {
+	log.Info("Setting up Menace 'Storat Command' command")
+	r.RegisterCommand("start", "Start a new Game of TicTacToe", NewGame)
+}
+
+func NewGame(s *discordgo.Session, m *discordgo.MessageCreate) {
+	s.ChannelMessageSend(m.ChannelID, "Not Implemented")
+}
