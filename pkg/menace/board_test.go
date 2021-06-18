7a9d239d462df6688f24cd7c7347946c811ea8da
Shaun Hammill <shammill@traumasoft.com>
2021-06-18 14:08:26 -0400

    More test cases

--- pkg/menace/board_test.go
+++ pkg/menace/board_test.go
@@ -3,7 +3,7 @@ package menace
 import "testing"
 
 func Test_BoardMatch(t *testing.T) {
-	t.Run("Test Matching Boards", func(t *testing.T) {
+	t.Run("Matching Boards", func(t *testing.T) {
 		var Board1 = NewBoard(X, X, X, O, EMPTY, O, X, X, X)
 		var Board2 = NewBoard(X, X, X, O, EMPTY, O, X, X, X)
 		if !BoardMatch(Board1, Board2) {
@@ -11,21 +11,21 @@ func Test_BoardMatch(t *testing.T) {
 		}
 
 	})
-	t.Run("Test Rotated Boards", func(t *testing.T) {
+	t.Run("Rotated Boards", func(t *testing.T) {
 		var Board1 = NewBoard(X, X, X, O, EMPTY, O, X, X, X)
 		var Board2 = NewBoard(X, O, X, X, EMPTY, X, X, O, X)
 		if !BoardMatch(Board1, Board2) {
 			t.Errorf("got %v want %v", false, true)
 		}
 	})
-	t.Run("Test Rotated Boards", func(t *testing.T) {
+	t.Run("Rotated Boards", func(t *testing.T) {
 		var Board1 = NewBoard(X, X, X, O, EMPTY, O, X, X, X)
 		var Board2 = NewBoard(O, X, X, X, EMPTY, X, O, X, X)
 		if BoardMatch(Board1, Board2) {
 			t.Errorf("got %v want %v", true, false)
 		}
 	})
-	t.Run("Test Mirrored Boards", func(t *testing.T) {
+	t.Run("Mirrored Boards", func(t *testing.T) {
 		var Board1 = NewBoard(
 			X, X, O,
 			X, EMPTY, O,
@@ -38,6 +38,22 @@ func Test_BoardMatch(t *testing.T) {
 		if !BoardMatch(Board1, Board2) {
 			t.Errorf("got %v want %v", false, true)
 		}
+	})
+	t.Run("Un Matched Boards", func(t *testing.T) {
+		var Board1 = NewBoard(X, X, X, O, EMPTY, O, X, X, X)
+		var Board2 = NewBoard(X, X, X, O, X, O, X, X, X)
+		if BoardMatch(Board1, Board2) {
+			t.Errorf("got %v want %v", true, false)
+		}
+	})
+
+	t.Run("Un Matched Boards", func(t *testing.T) {
+		var Board1 = NewBoard(X, X, X, O, EMPTY, O, X, X, X)
+		var Board2 = NewBoard(X, X, X, O, EMPTY, O, X, O, X)
+		if BoardMatch(Board1, Board2) {
+			t.Errorf("got %v want %v", true, false)
+		}
+
 	})
 }
 
